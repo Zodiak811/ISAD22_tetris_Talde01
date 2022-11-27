57f90de2ed0c61003ba65d7dd4da566d48b2d670
mikelgoje <83247194+mikelgoje@users.noreply.github.com>
2022-11-27 21:04:33 +0100

    Add files via upload

--- Logeatu.py
+++ Logeatu.py
@@ -34,6 +34,15 @@ def erabiltzaileaEzabatu(izena):
     cur.execute("DELETE FROM erabiltzaile WHERE izena=?", (izena,))
     con.commit()
 
+def pasahitzaLortu(izena):
+    con = sqlite3.connect("tutorial.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT pasahitza FROM erabiltzaile WHERE izena=?", (izena,))
+    pas = res.fetchone()
+    pasStr = ' '.join(pas)
+    return pasStr
+
+
 def login():
     def error(mezua):
         def bueltatu():
@@ -77,6 +86,55 @@ def login():
         error("Mesedez balio bat sartu")
 
 def datuakKonprobatu():
+    ################################## PASAHITZA BERRESKURATU ##################
+    def pasLortu():
+        def error(mezua):
+            def bueltatu():
+                window.destroy()
+
+            window = tk.Tk()
+            window.title("Errorea")
+            window.geometry("300x150+600+250")
+            lbl = tk.Label(window, text=mezua)
+            lbl.pack()
+            btn = tk.Button(window, text="Bueltatu", command=bueltatu)
+            btn.pack()
+        def berreskPasahitza():
+            db = open("database.txt", "r")
+            Username = sarIzen.get()
+            if not len(Username) < 1:
+                try:
+                    if erabiltzaileaDago(Username):
+                        try:
+                            print("Zure pasahitza " + pasahitzaLortu(Username) + " da")
+                            psh.configure(text="Zure pasahitza " + pasahitzaLortu(Username) + " da")
+                        except:
+                            print("Izena txarto sartu dituzu")
+                            error("Izena txarto sartu duzu")
+                    else:
+                        print("Erabiltzaile hori ez dago")
+                        error("Erabiltzaile hori ez dago")
+                except:
+                    print("Erabiltzailea ez da existitzen")
+                    error("Erabiltzailea ez da existitzen")
+            else:
+                print("Mesedez balio bat sartu")
+                error("Mesedez balio bat sartu")
+
+        window = tk.Tk()
+        window.title("Berreskuratzea")
+        window.geometry("300x150+600+250")
+        lbl = tk.Label(window, text="Sartu erabiltzaile izena: ")
+        lbl.pack()
+        sarIzen = tk.Entry(window)
+        sarIzen.pack()
+        btn = tk.Button(window, text="Berreskuratu", command=berreskPasahitza)
+        psh = tk.Label(window)
+        btn.pack()
+        buelt = tk.Button(window, text="Bueltatu", command=window.destroy)
+        psh.pack()
+        buelt.pack()
+#############################################END PASAHITZA BERRESKURATU##################
     global root
     root = tk.Tk()
     root.geometry("500x500+500+100")
@@ -89,6 +147,9 @@ def datuakKonprobatu():
     txt2=tk.Label(root,text="Sartu pasahitza: ")
     sar2=tk.Entry(root)
 
+    berreskuratu = tk.Button(root, text="Pasahitza ahaztu zait")
+    berreskuratu.configure(command=pasLortu)
+
     igo = tk.Button(root, text="Logeatu")
     igo.configure(command=login)
 
@@ -99,4 +160,5 @@ def datuakKonprobatu():
     sar2.pack()
 
     igo.pack()
+    berreskuratu.pack()
     root.mainloop()
